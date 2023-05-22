5ea29fd8eabb9a59c25ef9c02ce1445be2b175ba
yeira.putri <yeira.putri@ui.ac.id>
2023-05-22 15:27:32 +0700

    Authentication done

---
+++ peminjaman_stadium/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from peminjaman_stadium.views import *
+
+app_name = 'peminjaman_stadium'
+
+urlpatterns = [
+    path('', peminjaman_stadium, name='peminjaman_stadium'),
+    path('pilih_stadium/', pilih_stadium, name='pilih_stadium'),
+    path('list_waktu_stadium/', list_waktu_stadium, name='list_waktu_stadium')
+]
