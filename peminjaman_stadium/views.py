962ccc2781d7f8d18d180164d94a9181c74043f3
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2023-05-24 11:10:27 +0700

    Get data from db

--- peminjaman_stadium/views.py
+++ peminjaman_stadium/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from peminjaman_stadium.forms import PeminjamanStadiumForm
+from utils.query import query
 
 # Create your views here.
 def peminjaman_stadium(request):
@@ -23,11 +26,29 @@ def pilih_stadium(request):
     if role != "MANAJER":
         return redirect('home:home')
     
+    list_stadium, err = query("select id_stadium, nama from stadium")
+    
     context = {
-        "role": role
+        "role": role,
+        "list_stadium": list_stadium
     }
+
+    # gunakan form yang telah dibuat di forms.py
+    if request.method == 'POST':
+        form = PeminjamanStadiumForm(request.POST)
+        if form.is_valid():
+            # ambil data dari form
+            stadium = form.cleaned_data['stadium']
+            tanggal = form.cleaned_data['tanggal']
+            print(stadium)
+            print(tanggal)
+            # redirect ke halaman list waktu stadium
+            return redirect('peminjaman_stadium:list_waktu_stadium')
+        
+
     return render(request, 'pilih_stadium.html', context)
 
+@csrf_exempt
 def list_waktu_stadium(request):
     role = request.COOKIES.get('role')
 
