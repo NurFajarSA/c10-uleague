6daa6d0a12f6d574782040e6dfc1b0306b150e8d
yeira.putri <yeira.putri@ui.ac.id>
2023-05-25 21:02:17 +0700

    Finalized history_rapat

--- history_rapat/views.py
+++ history_rapat/views.py
@@ -57,14 +57,27 @@ def history_rapat(request):
 
         nama_panitia, err = query(f"SELECT CONCAT(np.nama_depan, ' ', np.nama_belakang) FROM non_pemain np JOIN panitia p ON p.id_panitia = np.id JOIN rapat r ON r.perwakilan_panitia = p.id_panitia where p.id_panitia = '{list_rapat[i][2]}'")
         nama_panitia = nama_panitia[0][0]
-        info_rapat.append([tim_rapat, nama_panitia, stadium, start_time, end_time])
+        laporan_rapat, err = query(f"select isi_rapat from rapat where id_pertandingan = '{list_pertandingan[i][0]}'")
+        info_rapat.append([tim_rapat, nama_panitia, stadium, start_time, end_time, laporan_rapat[0][0]])
         
     context = {
         "info_rapat": info_rapat,
+        
         "role": role
 
     }
-
-    print(info_rapat)
    
     return render(request, 'history_rapat.html', context)
+
+def isi_rapat(request,isi):
+    role = request.COOKIES.get('role')
+
+    if role == None:
+        return redirect('home:login')
+    if role not in ["MANAJER"]:
+        return redirect('home:home')
+    context = {
+        "isi_rapat": isi,
+        "role": role
+    }
+    return render(request, 'isi_rapat.html', context)
