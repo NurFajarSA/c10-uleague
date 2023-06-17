59c3749aa5b4e38f7e41306fbc20a90b5cb73127
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2023-06-17 18:11:08 +0700

    Setup deploy

--- c11_uleague/settings.py
+++ c11_uleague/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ub3gao^x9ykh6bqc51kcec(5s&4kh-%$m$r5cohquw(%vk$^ib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -143,6 +143,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
